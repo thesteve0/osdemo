a142aeb7aa054c746921732d693958a5574cc4c1
David Pierce <david.dean.pierce@gmail.com>
2012-09-14 09:29:18 -0700

    Move app code from app.js to server.js

--- server.js
+++ server.js
@@ -1,73 +1,48 @@
 #!/bin/env node
-//  OpenShift sample Node application
 
-var express = require('express');
-var fs      = require('fs');
+/**
+ * Module dependencies.
+ */
 
-//  Local cache for static content [fixed and loaded at startup]
-var zcache = { 'index.html': '' };
-zcache['index.html'] = fs.readFileSync('./index.html'); //  Cache index.html
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
 
-// Create "express" server.
-var app  = express.createServer();
-
-app.configure(function () {
-  app.use(express.static(__dirname + '/public'));
-});
-
-/*  =====================================================================  */
-/*  Setup route handlers.  */
-/*  =====================================================================  */
-
-// Handler for GET /health
-app.get('/health', function(req, res){
-    res.send('1');
-});
-
-// Handler for GET /asciimo
-app.get('/asciimo', function(req, res){
-    var link="https://a248.e.akamai.net/assets.github.com/img/d84f00f173afcf3bc81b4fad855e39838b23d8ff/687474703a2f2f696d6775722e636f6d2f6b6d626a422e706e67";
-    res.send("<html><body><img src='" + link + "'></body></html>");
-});
-
-// Handler for GET /
-app.get('/', function(req, res){
-    res.send(zcache['index.html'], {'Content-Type': 'text/html'});
-});
-
-app.get('/hello', function (req, res) {
-  res.send('Hello world', {'Content-Type': 'text/plain'});
-});
+var app = express();
 
 //  Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP;
-var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || 'localhost';
+var port    = process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080;
 
 if (typeof ipaddr === "undefined") {
    console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
 }
 
-//  terminator === the termination handler.
-function terminator(sig) {
-   if (typeof sig === "string") {
-      console.log('%s: Received %s - terminating Node server ...',
-                  Date(Date.now()), sig);
-      process.exit(1);
-   }
-   console.log('%s: Node server stopped.', Date(Date.now()) );
-}
-
-//  Process on exit and signals.
-process.on('exit', function() { terminator(); });
-
-['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
- 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
-].forEach(function(element, index, array) {
-    process.on(element, function() { terminator(element); });
+app.configure(function(){
+  app.set('port', port);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-//  And start the app on that interface (and port).
-app.listen(port, ipaddr, function() {
-   console.log('%s: Node server started on %s:%d ...', Date(Date.now() ),
-               ipaddr, port);
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+// Handler for GET /health
+app.get('/health', function(req, res){ res.send('1'); });
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
 });
