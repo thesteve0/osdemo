92747e4fc667709b584cc77da800caa7f6383d11
David Pierce <david.dean.pierce@gmail.com>
2012-09-14 09:08:35 -0700

    Set up backbone init code

--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,26 @@
 (function () {
+  var AppView;
+
+  AppView = Backbone.View.extend({
+    el: $('#appContainer'),
+    render: function () {
+      var template = _.template($('#appTemplate').html());
+      this.$el.html(template({}));
+      return true;
+    }
+  });
+
+
+  Router = Backbone.Router.extend({
+    initialize: function () {
+      var appView = new AppView();
+      appView.render();
+
+      Backbone.history.start();
+    }
+  });
+
+  $(function () {
+    var router = new Router();
+  });
 }());
